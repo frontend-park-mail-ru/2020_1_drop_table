7800134db75bc65ca2836a09cd9fbe6f817260b8
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-02-25 21:11:22 +0300

    Фикс регистрации

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -5,16 +5,16 @@ import './styles.css'
 import registerFormTemplate from './registerForm.hbs'
 import {validateForm} from "../../modules/formValidator";
 import {ajax} from "../../modules/ajax";
+import {createNewCafePage} from "../../main/main";
 
 
 export function renderRegister() {
-
 
     let registerContainer = document.createElement('div');
     registerContainer.className = "registerContainer";
     let topBar = document.createElement("div");
     topBar.className = "decorateContainer";
-    topBar.innerHTML = registerTemplate({name: 'Регистрация'})
+    topBar.innerHTML = registerTemplate({name: 'Регистрация'});
     registerContainer.appendChild(topBar);
 
     let form = document.createElement('div');
@@ -26,15 +26,25 @@ export function renderRegister() {
         repeatedPassword: 'Повторите пароль',
     })
     registerContainer.appendChild(form);
-    form = form.firstElementChild
+    form = form.firstElementChild;
     form.addEventListener('submit', function (e) {
-        e.preventDefault()
+        e.preventDefault();
+        //validateForm(form);
         const email = form.elements["email"].value;
         const password = form.elements["password"].value;
         const name = form.elements["full-name"].value;
-        ajax('POST', '/api/v1/owner', {name, email, password}, (response) => console.log(response)) //TODO ajax
-
-
+        console.log(email)
+        console.log(name)
+        ajax('POST', 'http://80.93.177.185/api/v1/owner',
+            {"name": name.toString(), "email": email.toString(), "password": password.toString()}
+            , (response) => {
+                console.log("RESPONSE", response);
+                if (response.errors === null) {
+                    console.log("all Ok")
+                } else {
+                    alert(response.errors[0].message)
+                }
+            });
     });
     return registerContainer
 
