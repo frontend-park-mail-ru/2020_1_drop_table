fe19178eca7649e36820deaccd74e5b32896d275
Elderly-AI <56202288+Elderly-AI@users.noreply.github.com>
2020-03-03 21:36:21 +0300

    router to class

--- src/main/main.js
+++ src/main/main.js
@@ -6,93 +6,89 @@ import {createUserProfilePage} from '../components/userProphilePage/creation';
 import {createNewCafePage} from '../components/AddCafePage/creation';
 
 
-const app = document.body;
+class Router{
 
+    constructor() {
+        this._routes = [];
+        this._app = document.body;
 
-let routes = [
-    {
-        url: '', callback: function () {
-            app.innerHTML = '';
-            app.appendChild(renderHeader());
-            createCafes();
-        }
+        this._initRoutes();
+        window.addEventListener('popstate', this._routing.bind(this));
+        setTimeout(this._routing.bind(this), 0);
     }
-];
 
-/**
- * Получает то, на что оканчивается url
- * @returns {string}
- */
-function getUrl() {
-    return window.location.hash.substr(1);
+    _getUrl() {
+        return window.location.hash.substr(1);
+    }
+
+    _routing() {
+        console.log(this);
+        const url = this._getUrl();
+        let route = this._routes[0];
+        this._routes.forEach(item => {
+            if (url === item.url) {
+                route = item;
+            }
+        });
+
+        route.callback();
+    }
+
+    _initRoutes(){
+        this._routes.push({
+            url: '', callback: () => {
+                this._app.innerHTML = '';
+                this._app.appendChild(renderBlankHeader());
+            }
+        });
+
+        this._routes.push({
+            url: 'reg', callback: () => {
+                this._app.innerHTML = '';
+                this._app.appendChild(renderBlankHeader());
+                this._app.appendChild(renderRegister());
+
+            }
+        });
+
+        this._routes.push({
+            url: 'login', callback: () => {
+                this._app.innerHTML = '';
+                this._app.appendChild(renderBlankHeader());
+                this._app.appendChild(renderLogin());
+            }
+
+        });
+
+        this._routes.push({
+            url: 'myCafe', callback: () => {
+                this._app.innerHTML = '';
+                this._app.appendChild(renderHeader());
+                createCafes();
+            }
+
+        });
+
+        this._routes.push({
+            url: 'profile', callback: () => {
+                this._app.innerHTML = '';
+                const up = document.createElement('div');
+                createUserProfilePage(up);
+                this._app.appendChild(renderHeader());
+                this._app.appendChild(up);
+            }
+
+        });
+
+        this._routes.push({
+            url: 'createCafe', callback: () => {
+                this._app.innerHTML = '';
+                this._app.appendChild(renderHeader());
+                createNewCafePage();
+            }
+
+        });
+    }
 }
 
-/**
- * При изменении url вызывается эта функция, она проходит по списку маршрутов и если подходящий роут найден,
- * вызывает его callback( какие функции нужно вызвать для рендеритнга страничкит)
- * @constructor
- */
-function Routing() { //Добавить в параметры e и вызывать e.preventDefault
-    let url = getUrl();
-    let route = routes[0];
-    routes.forEach(item => {
-        if (url === item.url) {
-            route = item;
-        }
-    });
-    route.callback();
-}
-
-
-window.addEventListener('popstate', Routing);
-
-
-setTimeout(Routing, 0);
-
-
-routes.push({
-
-    url: 'reg', callback: () => {
-        app.innerHTML = '';
-        app.appendChild(renderBlankHeader());
-        app.appendChild(renderRegister());
-
-    }
-});
-routes.push({
-    url: 'login', callback: () => {
-        app.innerHTML = '';
-        app.appendChild(renderBlankHeader());
-        app.appendChild(renderLogin());
-    }
-
-});
-
-routes.push({
-    url: 'myCafe', callback: () => {
-        app.innerHTML = '';
-        app.appendChild(renderHeader());
-        createCafes();
-    }
-
-});
-
-routes.push({
-    url: 'profile', callback: () => {
-        app.innerHTML = '';
-        let up = document.createElement('div');
-        createUserProfilePage(up);
-        app.appendChild(renderHeader());
-        app.appendChild(up);
-    }
-
-});
-
-routes.push({
-    url: 'createCafe', callback: () => {
-        app.innerHTML = '';
-        app.appendChild(renderHeader());
-        createNewCafePage();
-    }
-
-});
+let router = new Router;
