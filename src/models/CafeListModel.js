ffbe91c919ee61e07b9a94725b2b85acece1ef66
Elderly-AI <56202288+Elderly-AI@users.noreply.github.com>
2020-04-05 14:55:11 +0300

    jsdoc CafeListModel

--- src/models/CafeListModel.js
+++ src/models/CafeListModel.js
@@ -8,14 +8,20 @@ import {ajaxForm} from "../utils/ajaxForm";
 import {router} from "../main/main";
 import {authAjax} from "../utils/authAjax";
 
+/** Класс модели списка кафе */
 export default class CafeListModel{
 
+    /** Инициализация модели */
     constructor() {
         this._cafeModelsList = [];
         const cafeListData = this._loadCafeList();
         this._constructCafe(cafeListData);
     }
 
+    /**
+     * Возвращает промис, который возвращает сырое представление модели
+     * @return {Promise} промис, который возвращает сырое представление модели
+     */
     get context(){
         return new Promise(async (resolve) => {
             await this._checkCafeList();
@@ -27,7 +33,10 @@ export default class CafeListModel{
         });
     }
 
-
+    /**
+     * Возвращает промис, который возвращает пуст или не пуст cafeModelList
+     * @return {Promise} промис, который возвращает пуст или не пуст cafeModelList
+     */
     get isEmpty(){
         return new Promise(async (resolve) => {
             await this._checkCafeList();
@@ -35,18 +44,30 @@ export default class CafeListModel{
         });
     }
 
+    /**
+     * Возвращает cafe из cafeModelList, с нужным id
+     * @param {int} id кафе
+     * @return {CafeModel} объект CafeModel с нужным id
+     */
     getCafeById(id){
         return this._cafeModelsList.find((cafe) => {
             return cafe._id == id;
         });
     }
 
+    /**
+     * Проверяет существование поля data
+     * @param {string|null} data
+     */
     async _checkCafeList(data){
         if(!data){
             await this.cafesList();
         }
     }
 
+    /** Заполняет поля cafeModelList из sessionStorage
+     * @return {Array} Возвращает список кафе из CafelListModel
+     */
     _loadCafeList(){
         let cafeListData = sessionStorage.getItem('CafeList');
         if (cafeListData) {
@@ -58,10 +79,17 @@ export default class CafeListModel{
         }
     }
 
+    /**
+     * Сохраняет поля cafeListModel в sessionStorage
+     * @param {obj} data объект для сохранения
+     */
     _saveCafeList(data){
         sessionStorage.setItem('CafeList', JSON.stringify(data));
     }
 
+    /** Конструирует cafeModel из  cafeListData
+     * @param {Array} cafeListData сырое представление списка кафе
+     */
     _constructCafe(cafeListData){
         cafeListData.forEach((_, id) => {
             const cafe = new CafeModel(id);
@@ -69,10 +97,14 @@ export default class CafeListModel{
         });
     }
 
+    /** Создание кафе
+     * @return {CafeModel}
+     */
     createCafe(){
         return new CafeModel();
     }
 
+    /** Получение списка кафе */
     async cafesList() {
         await ajax(constants.PATH + '/api/v1/cafe',
             'GET',
@@ -92,6 +124,7 @@ export default class CafeListModel{
         )
     }
 
+    /** Создание кафе */
     async create(photo, cafe) {
         await ajaxForm(constants.PATH + '/api/v1/cafe',
             'POST',
@@ -111,7 +144,7 @@ export default class CafeListModel{
         );
     }
 
-
+    /** Изменение кафе */
     async editCafe(photo = null, cafe, id){
         await ajaxForm(constants.PATH + `/api/v1/cafe/${id}`,
             'PUT',
@@ -125,8 +158,5 @@ export default class CafeListModel{
                 }
             }
         );
-
     }
-
-
 }
