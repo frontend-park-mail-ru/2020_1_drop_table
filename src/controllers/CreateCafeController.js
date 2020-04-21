57d73651d00d7f3e3f28dc55beda802c93421b8a
Elderly-AI <56202288+Elderly-AI@users.noreply.github.com>
2020-04-21 14:35:12 +0300

    regex fix

--- src/controllers/CreateCafeController.js
+++ src/controllers/CreateCafeController.js
@@ -178,8 +178,8 @@ export default class CreateCafeController{
             {
                 element: form.elements['openTime'],
                 validate: () => {
-                    const timeRegex = /([0-1][0-9])|(2[0-4])*/;
-                    if(timeRegex.test(form.elements['openTime'].value.toString())){
+                    const timeRegex = /^([0-1][0-9])|(2[0-3]):([0-5][0-9])$/;
+                    if(!timeRegex.test(form.elements['openTime'].value.toString())){
                         return 'Время имеет некорректный формат';
                     }
                 }
@@ -187,8 +187,8 @@ export default class CreateCafeController{
             {
                 element: form.elements['closeTime'],
                 validate: () => {
-                    const timeRegex = /([0-1][0-9])|(2[0-4])*/;
-                    if(timeRegex.test(form.elements['closeTime'].value.toString())){
+                    const timeRegex = /^([0-1][0-9])|(2[0-3]):([0-5][0-9])$/;
+                    if(!timeRegex.test(form.elements['closeTime'].value.toString())){
                         return 'Время имеет некорректный формат';
                     }
                 }
