ccd33bb70ba2b52d645ecf050ab19a30dbee40e5
Elderly-AI <56202288+Elderly-AI@users.noreply.github.com>
2020-05-14 00:19:42 +0300

    yandex ,ap

--- src/models/LandingModel.js
+++ src/models/LandingModel.js
@@ -1,7 +1,25 @@
 'use strict';
 
+import {ajax} from '../utils/ajax';
+import {constants} from '../utils/constants';
+
 /** Модель лендинга (пока статическая) */
 export default class LandingModel { // Пока пустой и статичный
     constructor() {
+        this.cafes = [];
+    }
+
+    async getNearestCafes(latitude, longitude, radius) {
+        await ajax(constants.PATH + `/api/v1/cafe/get_by_geo?latitude=${latitude}&longitude=${longitude}&radius=${radius}`,
+            'GET',
+            {},
+            (response) => {
+                if(response.errors === null){
+                    this.cafes = response.data ? response.data : [];
+                    return
+                }
+                throw response.errors;
+            }
+        )
     }
 }
